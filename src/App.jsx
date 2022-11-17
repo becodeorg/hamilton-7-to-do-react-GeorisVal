92b9e86ba0e2d86d90c3fd98751982c46bc28dde
GeorisVal <georis.val@gmail.com>
2022-11-17 21:54:33 +0100

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-const title = <h1 class="text-2xl">My To-Do List</h1>
-const inputText = <input type="text" placeholder="What needs to be done ?" class="border border-emerald-900 p-1 pl-2"></input>
-const button = <button class="bg-blue-500 text-white w-52 h-10 m-5">Add task</button>
-const checkbox = (`${<input type="checkbox" class="mr-2"></input>}
-${<label for="checkbox">CB</label>}`);
+
+const title = <h1 class="text-2xl dark:text-white">My To-Do List</h1>
+const inputText = <input type="text" placeholder="What needs to be done ?" class="border border-emerald-900 dark:border-white p-1 pl-2"></input>
+const button = <button class="bg-blue-500 text-white w-52 h-10 m-5 hover:bg-blue-800">Add task</button>
+const checkbox = <input type="checkbox" class="mr-2"></input>;
+const label =  <label for="checkbox" class="dark:text-white">CB</label>
 
 function App() {
-
+  const [theme, setTheme] = useState('light');
   return (
-    <div class="h-screen w-screen flex flex-col justify-start">
-      <header class="h-20 bg-slate-100 border-b-2 flex items-center justify-center">
+    <div class="-screen w-screen flex flex-col justify-start dark:bg-slate-600">
+      <header class="h-20 bg-slate-100 dark:bg-slate-900 border-b-2 flex items-center justify-center">
         {title}
       </header>
       <div class="mt-5 ml-5">
         {inputText}<br />
         {button}<br />
-        {checkbox}
+        {checkbox} {label}
       </div>
     </div>
   )
