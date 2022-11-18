11a976d5a2f8883f9bca32f8ad7d0cca39ee56ea
GeorisVal <georis.val@gmail.com>
2022-11-18 23:36:08 +0100

    Renamed checkbox.jsx to todo.jsx, improving array

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,13 +1,20 @@
 import React, {useState} from "react";
 
-export default function Checkbox() {
+export default function ToDO() {
     const initialTodos = ["First thing first", "Second thing second"];
-    const [todos, setTodos] = useState(initialTodos)
+    const toDoList = [
+        {
+            id: id,
+            task: Task,
+            complete: false
+        }
+    ];
+    const [todos, setTodos] = useState(toDoList);
     return ( 
         <ul>
             {todos.map((todo) => (
                 <li key={todo}>
-                    <input type="checkbox" className="mr-2" onChange={e => setTodos(e)}></input>
+                    <input type="checkbox" className="mr-2" onChange={e => initialTodos.pop(e)}></input>
                     <label for="checkbox" className="dark:text-white">{todo}</label>
                 </li>
             ))}
