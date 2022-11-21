07826c3bf98b918d27112dea5c2eb277b7b14110
GeorisVal <georis.val@gmail.com>
2022-11-21 20:44:07 +0100

    Added Remove items buttons

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 export default function ToDo(props) {
     let toDo = props.todo;    
-
+    let locStorage = 'toDoApp'
     const handleChange = (id) => {
         const newToDo = todos.map((todo) => {
             if (todo.id === id) {
@@ -18,18 +18,32 @@ export default function ToDo(props) {
 
     React.useEffect(() => {
         setTodos(props.todo);
-    })
+        window.localStorage.setItem(locStorage + ".todos", JSON.stringify(toDo))});
+    
 
     return ( 
-        <ul className="grid grid-cols-2 ml-auto mr-auto gap-10">
-            {todos.map((todo) => (
-            <li key={todo.id}>
-                <input type="checkbox" id = {todo.id} value={todo.done} onChange={() => {
-                    handleChange(todo.id)
-                }} className="accent-blue-300 scale-[3] m-10"></input>
-                <label htmlFor={todo.id} className={`dark:text-white ml-5 text-xl ${todo.done?"line-through":"no-underline"}`}>{todo.title}</label>
-            </li>
-            ))}
-        </ul>
+        <div className="flex flex-col justify-center items-center w-full">
+            <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 sm:ml-auto mr-auto gap-10">
+                {todos.map((todo) => (
+                <li key={todo.id}>
+                    <input type="checkbox" id = {todo.id} value={todo.done} onChange={() => {
+                        handleChange(todo.id)
+                    }} className="accent-blue-300 scale-[3] m-10"></input>
+                    <label htmlFor={todo.id} className={`dark:text-white ml-5 text-xl ${todo.done?"line-through":"no-underline"}`}>{todo.title}</label>
+                </li>
+                ))}
+            </ul>
+            <div className="flex w-full">
+                <button className="bg-blue-500 text-white h-10 m-5 hover:bg-blue-800 rounded-md w-[45%]">Wemove Completed</button> 
+                <button className="bg-blue-500 text-white h-10 m-5 hover:bg-blue-800 rounded-md w-[45%]"onClick={
+                    () => {
+                        if(window.confirm('This will completely remove all the things to do. Are you sure you want to continue ?\nThis action cannot be undone.')) {
+                        window.localStorage.removeItem(locStorage + ".todos");
+                        window.location.reload(false);
+                        }
+                }
+                }>Wemove All</button>
+            </div>
+        </div>
     )
 }
