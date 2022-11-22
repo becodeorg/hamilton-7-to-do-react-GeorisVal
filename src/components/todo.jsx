148613d24b5640082de4e7e02ffad184facfffa7
GeorisVal <georis.val@gmail.com>
2022-11-22 11:09:36 +0100

    Added Tooltip on disabled button

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -33,7 +33,7 @@ export default function ToDo(props) {
                 ))}
             </ul>
             <div className={`flex w-full align-center justify-center ${toDo.length === 0 ?"hidden":""}`}>
-                <button className="bg-blue-500 text-white h-10 m-5 hover:bg-blue-800 rounded-md w-[45%] disabled:bg-gray-800" disabled onClick={
+                <button className="bg-blue-500 text-white h-10 m-5 hover:bg-blue-800 rounded-md w-[45%] disabled:bg-gray-800" disabled title="Coming Soon™" onClick={
                     () => {
                         if(window.confirm('This will completely remove all the things done. Are you sure you want to continue ?\nThis action cannot be undone.')) {
                         window.localStorage.removeItem(locStorage + ".todos");
