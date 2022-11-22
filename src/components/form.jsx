0585cca366df4d3e0d8cc44a387ab0af72c5ab82
GeorisVal <georis.val@gmail.com>
2022-11-22 09:35:40 +0100

    Added sending new task on Enter

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -12,9 +12,20 @@ export default function Form(props) {
             inputElement.value = '';
         }
     }
+    function enterHandler(e) {
+        if(e.key === "Enter") {
+            const inputElement = inputRef.current
+            if(inputElement.value == '') {
+                alert("Come on, I'm sure you've got stuff to do.");
+            }else {
+                props.addTodo(inputElement.value)
+                inputElement.value = '';
+            }
+        }
+    }
     return (
         <div className="w-full flex flex-col ml-auto mr-auto">
-            <input ref={inputRef} type="text" placeholder="What needs to be done ?" className="border border-emerald-900 dark:border-white p-1 pl-2 rounded-md w-full"></input>
+            <input ref={inputRef} type="text" placeholder="What needs to be done ?" className="border border-emerald-900 dark:border-white p-1 pl-2 rounded-md w-full" onKeyUp={enterHandler}></input>
             <button className="bg-blue-500 text-white h-10 m-5 hover:bg-blue-800 rounded-md w-full" onClick={clickHandler}>Add task</button>
         </div>
     )
