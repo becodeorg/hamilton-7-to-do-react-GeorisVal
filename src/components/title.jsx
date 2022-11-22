08089fb4971ee4be43ade261d45ddab8ca454fb2
GeorisVal <georis.val@gmail.com>
2022-11-22 10:14:09 +0100

    Increased font size

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,7 +1,7 @@
 export default function Title() {
     return ( 
         <header className="h-20 bg-slate-100 dark:bg-slate-900 border-b-2 flex items-center justify-center">  
-            <h1 className="text-2xl dark:text-white">My To-Do List</h1>
+            <h1 className="text-5xl dark:text-white">My To-Do List</h1>
         </header>
     )
 }
